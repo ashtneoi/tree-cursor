73dc3f0dd80a011fb5d7a4bfe7edb72facc8df2f
ashtneoi <ashtneoi@gmail.com>
2018-05-29 12:05:40 -0700

    Get rid of cursor traits

--- src/cursor.rs
+++ src/cursor.rs
@@ -50,14 +50,12 @@ impl<'n, N: 'n + Down> TreeCursor<'n, N> {
         let new_ptr = self.down_map_ptr(f)?;
         Some(Self::new(unsafe { new_ptr.as_ref().unwrap() }))
     }
-}
 
-impl<'n, N: 'n + Down> OpaqueVerticalCursor for TreeCursor<'n, N> {
-    fn zero(&mut self) {
+    pub fn zero(&mut self) {
         self.stack.last_mut().unwrap().1 = 0;
     }
 
-    fn down(&mut self) -> bool {
+    pub fn down(&mut self) -> bool {
         let maybe_new_ptr = self.down_ptr();
         if let &Some(new_ptr) = &maybe_new_ptr {
             self.stack.push((new_ptr, 0));
@@ -65,7 +63,7 @@ impl<'n, N: 'n + Down> OpaqueVerticalCursor for TreeCursor<'n, N> {
         maybe_new_ptr.is_some()
     }
 
-    fn up(&mut self) -> bool {
+    pub fn up(&mut self) -> bool {
         if self.stack.len() == 1 {
             self.stack[0].1 = 0;
             false
@@ -74,17 +72,13 @@ impl<'n, N: 'n + Down> OpaqueVerticalCursor for TreeCursor<'n, N> {
             true
         }
     }
-}
 
-impl<'n, N: 'n + Down> VerticalCursor for TreeCursor<'n, N> {
-    type Item = N;
-
-    fn get(&self) -> &N {
+    pub fn get(&self) -> &N {
         let here: *const N = self.stack.last().unwrap().0;
         unsafe { here.as_ref().unwrap() }
     }
 
-    fn down_new(&mut self) -> Option<Self> {
+    pub fn down_new(&mut self) -> Option<Self> {
         let new_ptr = self.down_ptr()?;
         Some(Self::new(unsafe { new_ptr.as_ref().unwrap() }))
     }
@@ -140,14 +134,12 @@ impl<'n, N: 'n + DownMut> TreeCursorMut<'n, N> {
         let new_ptr = self.down_map_ptr(f)?;
         Some(Self::new(unsafe { new_ptr.as_mut().unwrap() }))
     }
-}
 
-impl<'n, N: 'n + DownMut> OpaqueVerticalCursor for TreeCursorMut<'n, N> {
-    fn zero(&mut self) {
+    pub fn zero(&mut self) {
         self.stack.last_mut().unwrap().1 = 0;
     }
 
-    fn down(&mut self) -> bool {
+    pub fn down(&mut self) -> bool {
         let maybe_new_ptr = self.down_ptr();
         if let &Some(new_ptr) = &maybe_new_ptr {
             self.stack.push((new_ptr, 0));
@@ -155,7 +147,7 @@ impl<'n, N: 'n + DownMut> OpaqueVerticalCursor for TreeCursorMut<'n, N> {
         maybe_new_ptr.is_some()
     }
 
-    fn up(&mut self) -> bool {
+    pub fn up(&mut self) -> bool {
         if self.stack.len() == 1 {
             self.stack[0].1 = 0;
             false
@@ -164,24 +156,18 @@ impl<'n, N: 'n + DownMut> OpaqueVerticalCursor for TreeCursorMut<'n, N> {
             true
         }
     }
-}
 
-impl<'n, N: 'n + DownMut> VerticalCursor for TreeCursorMut<'n, N> {
-    type Item = N;
-
-    fn get(&self) -> &N {
+    pub fn get(&self) -> &N {
         let here: *const N = self.stack.last().unwrap().0;
         (unsafe { here.as_ref() }).unwrap()
     }
 
-    fn down_new(&mut self) -> Option<Self> {
+    pub fn down_new(&mut self) -> Option<Self> {
         let new_ptr = self.down_ptr()?;
         Some(Self::new(unsafe { new_ptr.as_mut().unwrap() }))
     }
-}
 
-impl<'n, N: 'n + DownMut> VerticalCursorMut for TreeCursorMut<'n, N> {
-    fn get_mut(&mut self) -> &mut N {
+    pub fn get_mut(&mut self) -> &mut N {
         let here = self.stack.last().unwrap().0;
         (unsafe { here.as_mut() }).unwrap()
     }
--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ pub mod cursor;
 
 pub mod prelude {
     pub use super::{
-        Down, DownMut, OpaqueVerticalCursor, VerticalCursor, VerticalCursorMut,
+        Down, DownMut,
     };
 }
 
@@ -15,24 +15,4 @@ pub trait Down {
 
 pub trait DownMut {
     fn down_mut(&mut self, idx: usize) -> Option<&mut Self>;
-}
-
-pub trait OpaqueVerticalCursor {
-    fn zero(&mut self);
-    fn down(&mut self) -> bool;
-    fn up(&mut self) -> bool;
-}
-
-pub trait VerticalCursor: OpaqueVerticalCursor
-where
-    Self: Sized,
-{
-    type Item;
-
-    fn get(&self) -> &Self::Item;
-    fn down_new(&mut self) -> Option<Self>;
-}
-
-pub trait VerticalCursorMut: VerticalCursor {
-    fn get_mut(&mut self) -> &mut Self::Item;
 }
